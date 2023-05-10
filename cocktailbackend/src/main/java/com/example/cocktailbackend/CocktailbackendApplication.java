5f8fdef25e899aa3ad95d7304f4ee85b4b115f17
3956ray <3956ray@gmail.com>
2023-05-10 20:48:13 +0800

    Create frontend and backend

---
+++ cocktailbackend/src/main/java/com/example/cocktailbackend/CocktailbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cocktailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailbackendApplication.class, args);
+    }
+
+}
